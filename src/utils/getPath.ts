e7cdae1220b24e2a9c64c75d54fbb46e1bf74097
Yuto17 <civalevincenzoyuto@gmail.com>
2025-12-27 12:20:22 +0100

    refactor: simplify getPath function and improve parameter naming

--- src/utils/getPath.ts
+++ src/utils/getPath.ts
@@ -1,36 +1,29 @@
-import { BLOG_PATH } from "@/content.config";
 import { slugifyStr } from "./slugify";
 
 /**
  * Get full path of a blog post
  * @param id - id of the blog post (aka slug)
- * @param filePath - the blog post full file location
+ * @param _filePath - the blog post full file location (unused, but kept for compatibility)
  * @param includeBase - whether to include `/posts` in return value
  * @returns blog post path
  */
 export function getPath(
   id: string,
-  filePath: string | undefined,
+  _filePath: string | undefined,
   includeBase = true
 ) {
-  const pathSegments = filePath
-    ?.replace(BLOG_PATH, "")
-    .split("/")
-    .filter(path => path !== "") // remove empty string in the segments ["", "other-path"] <- empty string will be removed
-    .filter(path => !path.startsWith("_")) // exclude directories start with underscore "_"
-    .slice(0, -1) // remove the last segment_ file name_ since it's unnecessary
-    .map(segment => slugifyStr(segment)); // slugify each segment path
-
   const basePath = includeBase ? "/posts" : "";
 
-  // Making sure `id` does not contain the directory
-  const blogId = id.split("/");
-  const slug = blogId.length > 0 ? blogId.slice(-1) : blogId;
+  // id is like "2025/my-post.md"
+  // we want to get "/posts/2025/my-post"
+  const slugWithExtension = id;
 
-  // If not inside the sub-dir, simply return the file path
-  if (!pathSegments || pathSegments.length < 1) {
-    return [basePath, slug].join("/");
-  }
+  // Find the last dot and remove the extension
+  const lastDotIndex = slugWithExtension.lastIndexOf('.');
+  const slugWithoutExtension = lastDotIndex > -1 ? slugWithExtension.substring(0, lastDotIndex) : slugWithExtension;
 
-  return [basePath, ...pathSegments, slug].join("/");
+  // slugify parts
+  const slugParts = slugWithoutExtension.split("/").map(part => slugifyStr(part));
+
+  return [basePath, ...slugParts].join("/");
 }
